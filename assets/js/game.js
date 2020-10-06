766a2556500424d9d370475ee3ef8a3efaf4a1cc
Joe-Louis D'Santos <joelouis14@hotmail.co.uk>
2020-10-06 13:09:23 +0000

    restarted game.js as couldnt connect to windows, made sure pdf for cv was linked to all pages and added divs to game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,40 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll ('.grid div')
+    const scoreDisplay = document.querySelector('span')
+    const startBtn = document.querySelector('.start')
 
+    const width = 10
+    let currentIndex = 0
+    let foodIndex = 0
+    let currentSnake = [2,1,0] // grid 2 is the head, grid 0 is the tail and grid 1 is the body
+    let direction = 1
+    let score = 0
+    let speed = 0.9
+    let intervalTime = 0
+    let interval = 0
 
+    function control(e) {
+        squares[currentIndex].classList.remove('snake')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*var character = document.getElementById("character");
-var block = document.getElementById("block");
-function jump() {
-    if(character.classList != "animate"){
-    character.classList.add("animate");
+        if(e.keycode === 39) {
+            direction = 1 // pressing right on keyboard, snake goes right
+        } else if (e.keycode === 38) {
+            direction = -width // pressing up arrow, snake will go back 10 divs appearing to go up
+        } else if (e.keycode === 37) {
+            direction = -1 // if pressing left arrow, snake goes left 1
+        } else if (e.keycode === 40) {
+            direction = +width // pressing the down arrow moves the snake 10 divs forward making it look like it goes down
+        }
     }
-    setTimeout(function(){
-        character.classList.remove("animate")
-    }, 500);
-}
 
-var checkDeath = setInterval(function(){
-    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if (blockLeft<20 && blockLeft>0 && characterTop>=130){
-        block.style.animation = "none";
-        block.style.display = "none";
-        alert("Ouch!! Nice try!! Refresh and try again")
-    } 
-},10);*/
+    document.addEventListener('keyup', control)
 
+})
